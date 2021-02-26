e087515be0dc2deddb76e616d657744d3b69d721
nelson9503 <nelson9503@hotmail.com>
2021-02-26 12:22:20 +0800

    refactor: rewrite codes to simplify usage

---
+++ object_database.py
@@ -0,0 +1,133 @@
+import os
+import json
+import pymysql
+from tkinter import messagebox, simpledialog
+from .object_table import TB
+
+
+class DB:
+
+    def __init__(self, db_name: str):
+        """
+        Connect to a database.
+        """
+        self.db_name = db_name
+        self.__connect_to_server()
+        self.__check_db_exists()
+
+    def close(self):
+        """
+        Close connection.
+        """
+        self.conn.close()
+
+    def drop(self):
+        """
+        Drop database from server.
+        """
+        sql = "DROP DATABASE {};".format(self.db_name)
+        cursor = self.conn.cursor()
+        cursor.execute(sql)
+
+    def list_db(self) -> list:
+        """
+        List out all databases in mysql server.
+        """
+        sql = "SELECT schema_name FROM information_schema.schemata;"
+        dbs = []
+        cursor = self.conn.cursor()
+        cursor.execute(sql)
+        results = cursor.fetchall()
+        for row in results:
+            for val in row:
+                dbs.append(val)
+        return dbs
+
+    def list_tb(self) -> list:
+        """
+        List out all tables in the database.
+        """
+        sql = "SHOW TABLES FROM `{}`".format(self.db_name)
+        tbs = []
+        cursor = self.conn.cursor()
+        cursor.execute(sql)
+        results = cursor.fetchall()
+        for row in results:
+            for val in row:
+                tbs.append(val)
+        return tbs
+
+    def add_tb(self, tb_name: str, key_col_name: str, key_data_type: str) -> object:
+        sql = "CREATE TABLE `{}`.`{}` ".format(self.db_name, tb_name)
+        sql += "(`{}` {})".format(key_col_name,
+                                  key_data_type+" NOT NULL PRIMARY KEY")
+        cursor = self.conn.cursor()
+        cursor.execute(sql)
+        tb = TB(tb_name, self)
+        return tb
+
+    def TB(self, tb_name: str) -> object:
+        tb = TB(tb_name, self)
+        return tb
+
+    def execute(self, sql_quote: str) -> dict:
+        """
+        Execute the sql quote.
+        """
+        cursor = self.conn.cursor(pymysql.cursors.DictCursor)
+        cursor.execute(sql_quote)
+        results = cursor.fetchall()
+        return results
+
+    def __check_db_exists(self):
+        if not self.db_name in self.list_db():
+            sql = "CREATE DATABASE {};".format(self.db_name)
+            cursor = self.conn.cursor()
+            cursor.execute(sql)
+
+    def __connect_to_server(self):
+        while True:
+            try:
+                self.__get_configs()
+                self.conn = pymysql.connect(
+                    host=self.config["host"],
+                    port=self.config["port"],
+                    user=self.config["user"],
+                    password=self.config["password"]
+                )
+                break
+            except:
+                messagebox.showerror(
+                    title="MySQL Login Failed",
+                    message="Failed to login MySQL.\nPlease reconfig login information."
+                )
+                host = simpledialog.askstring(
+                    "MySQL Configuration", "Host Name:")
+                port = simpledialog.askinteger("MySQL Configuration", "Port:")
+                user = simpledialog.askstring(
+                    "MySQL Configuration", "User Name:")
+                password = simpledialog.askstring(
+                    "MySQL Configuration", "Password:", show="*")
+                with open(self.config_path, 'w') as f:
+                    j = {
+                        "host": host,
+                        "port": port,
+                        "user": user,
+                        "password": password
+                    }
+                    with open(self.config_path, 'w') as f:
+                        f.write(json.dumps(j, indent=4))
+
+    def __get_configs(self) -> dict:
+        self.config_path = "./mysql_config.json"
+        if not os.path.exists(self.config_path):
+            j = {
+                "host": None,
+                "port": None,
+                "user": None,
+                "password": None
+            }
+            with open(self.config_path, 'w') as f:
+                f.write(json.dumps(j, indent=4))
+        with open(self.config_path, 'r') as f:
+            self.config = json.loads(f.read())
---
+++ object_table.py
@@ -0,0 +1,147 @@
+import pymysql
+
+
+class TB:
+
+    def __init__(self, table_name: str, DB: object):
+        self.DB = DB
+        self.tb_name = table_name
+        self.key_col_name = self.__get_key_col_name()
+
+    def drop(self):
+        """
+        Drop table from the database.
+        """
+        sql = "DROP TABLE `{}`.`{}`;".format(self.DB.db_name, self.tb_name)
+        cursor = self.DB.conn.cursor()
+        cursor.execute(sql)
+
+    def rename(self, new_tb_name: str):
+        """
+        Rename table name.
+        """
+        cursor = self.DB.conn.cursor()
+        cursor.execute(
+            "ALTER TABLE `{db}`.`{tb}` RENAME `{db}`.`{new_tb}`;".format(
+                db=self.DB.db_name,
+                tb=self.tb_name,
+                new_tb=new_tb_name
+            )
+        )
+        self.tb_name = new_tb_name
+
+    def list_col(self) -> list:
+        """
+        List out columns in the table.
+        """
+        sql = "SHOW COLUMNS FROM `{}`.`{}`;".format(
+            self.DB.db_name, self.tb_name)
+        cols = []
+        cursor = self.DB.conn.cursor(pymysql.cursors.DictCursor)
+        cursor.execute(sql)
+        results = cursor.fetchall()
+        for row in results:
+            cols.append(row["Field"])
+        return cols
+
+    def add_col(self, col_name: str, data_type: str):
+        """
+        Add a new column to the table.
+        """
+        sql = "ALTER TABLE `{}`.`{}` ADD `{}` {};".format(
+            self.DB.db_name, self.tb_name, col_name, data_type)
+        cursor = self.DB.conn.cursor()
+        cursor.execute(sql)
+
+    def drop_col(self, col_name: str):
+        """
+        Drop the column from the table.
+        """
+        sql = "ALTER TABLE `{}`.`{}` DROP `{}`;".format(
+            self.DB.db_name, self.tb_name, col_name)
+        cursor = self.DB.conn.cursor()
+        cursor.execute(sql)
+
+    def drop_data(self, key_value: any):
+        """
+        Drop a row of data by using key.
+        """
+        sql = "DELETE FROM `{}`.`{}` WHERE `{}`={};".format(
+            self.DB.db_name, self.tb_name, self.key_col_name, key_value)
+        cursor = self.DB.conn.cursor()
+        cursor.execute(sql)
+
+    def query(self, column: str = "*", condition="") -> dict:
+        """
+        Query the table and return results in dictionary.
+        """
+        sql = "SELECT {} FROM `{}`.`{}`".format(
+            column, self.DB.db_name, self.tb_name)
+        if not condition == "":
+            sql += " " + condition
+        cursor = self.DB.conn.cursor(pymysql.cursors.DictCursor)
+        cursor.execute(sql)
+        records = cursor.fetchall()
+        results = {}
+        for i in range(len(records)):
+            results[i] = records[i]
+        return results
+
+    def update(self, data: dict):
+        """
+        Insert data for data with new keys.
+        Update data for data with existed keys.
+        """
+        if len(data) == 0:
+            return False
+        # part 1
+        columns = data[0].keys()
+        part1 = "("
+        for column in columns:
+            part1 += "`" + column + "`,"
+        if part1[-1] == ',':
+            part1 = part1[:-1]
+        part1 += ")"
+        # part 2
+        part2 = ""
+        for key in data:
+            row = data[key]
+            part2 += "("
+            for column in columns:
+                val = row[column]
+                if type(val) == str:
+                    val = "'" + val + "'"
+                part2 += str(val) + ","
+            if part2[-1] == ',':
+                part2 = part2[:-1]
+            part2 += "),"
+        if part2[-1] == ',':
+            part2 = part2[:-1]
+        # part 3
+        key = self.key_col_name
+        if key == False:
+            raise AttributeError("Cannot control a table without key column.")
+        part3 = ""
+        for column in columns:
+            if column == key:
+                part3 += "`" + column + "` = VALUES(`" + column + "`),"
+        if part3[-1] == ',':
+            part3 = part3[:-1]
+        # group parts
+        sql = "INSERT INTO `{}`.`{}` ".format(self.DB.db_name, self.tb_name)
+        sql += part1 + " VALUES "
+        sql += part2 + " ON DUPLICATE KEY UPDATE "
+        sql += part3 + ";"
+        cursor = self.DB.conn.cursor(pymysql.cursors.DictCursor)
+        cursor.execute(sql)
+
+    def __get_key_col_name(self) -> str:
+        sql = "SHOW COLUMNS FROM `{}`.`{}`;".format(
+            self.DB.db_name, self.tb_name)
+        cursor = self.DB.conn.cursor(pymysql.cursors.DictCursor)
+        cursor.execute(sql)
+        results = cursor.fetchall()
+        for row in results:
+            if row["Key"] == "PRI":
+                return row["Field"]
+        return False
